a2d85edc8c5d4bf429f36365bb61ef43e4705cfa
Andi Purnomo <106710988+andiarz@users.noreply.github.com>
2023-04-05 07:33:44 +0700

    Update login.php

--- web_bootstrap/admin/login.php
+++ web_bootstrap/admin/login.php
@@ -3,6 +3,31 @@
     include_once 'menu.php';
 ?>
 
+<?php
+
+// Memulai session
+session_start();
+
+// Cek apakah form login sudah di-submit
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+  // Simpan username dan password dari form
+  $username = $_POST['email'];
+  $password = $_POST['password'];
+
+  // Lakukan pengecekan username dan password
+  if ($username == 'admin@gmail.com' && $password == '123456') {
+    // Jika username dan password benar, set session dan redirect ke halaman utama
+    $_SESSION['email'] = $username;
+    header('Location: berita.php');
+    exit;
+  } else {
+    // Jika username atau password salah, tampilkan pesan error
+    $error = 'Email atau password salah';
+  }
+}
+
+?>
+
 <div id="layoutSidenav_content">
     <div id="layoutAuthentication">
             <div id="layoutAuthentication_content">
@@ -11,15 +36,18 @@
                         <div class="row justify-content-center">
                             <div class="col-lg-5">
                                 <div class="card shadow-lg border-0 rounded-lg mt-5">
-                                    <div class="card-header"><h3 class="text-center font-weight-light my-4">Login</h3></div>
+                                <div class="card-header"><h3 class="text-center font-weight-light my-4">Login</h3></div>
+                                    <?php if(isset($error)) : ?>
+                                        <div style="color: red;"><?php echo $error;?></div>
+                                    <?php endif; ?>
                                     <div class="card-body">
-                                        <form>
+                                        <form method="POST">
                                             <div class="form-floating mb-3">
-                                                <input class="form-control" id="inputEmail" type="email" placeholder="name@example.com" />
+                                                <input class="form-control" id="inputEmail" type="email" placeholder="name@example.com" name="email"/>
                                                 <label for="inputEmail">Email address</label>
                                             </div>
                                             <div class="form-floating mb-3">
-                                                <input class="form-control" id="inputPassword" type="password" placeholder="Password" />
+                                                <input class="form-control" id="inputPassword" type="password" placeholder="Password" name="password"/>
                                                 <label for="inputPassword">Password</label>
                                             </div>
                                             <div class="form-check mb-3">
@@ -28,7 +56,7 @@
                                             </div>
                                             <div class="d-flex align-items-center justify-content-between mt-4 mb-0">
                                                 <a class="small" href="password.html">Forgot Password?</a>
-                                                <a class="btn btn-primary" href="index.html">Login</a>
+                                                <a class="btn btn-primary" type="submit" href="index.html">Login</a>
                                             </div>
                                         </form>
                                     </div>
