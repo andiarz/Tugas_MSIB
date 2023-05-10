0ec76d55d46c7e36c9d5145da4a671812377872f
Andi Purnomo <106710988+andiarz@users.noreply.github.com>
2023-05-11 02:16:39 +0700

    Update kartu_detail.php

--- TugasPHP/web_native-master/admin/kartu_detail.php
+++ TugasPHP/web_native-master/admin/kartu_detail.php
@@ -5,9 +5,26 @@ $kartu = $model->getKartu($id);
 
 ?>
 
-<div>
-    <h3><?= $kartu['kode'] ?> </h3>
-    <h3><?= $kartu['nama'] ?> </h3>
-    <h3><?= $kartu['diskon'] ?> </h3>
-    <h3><?= $kartu['iuran'] ?> </h3>
+<h1 class="mt-4">Tabel Detail Kartu Member</h1>
+<div class="card-body">
+    <div class="card mb-4">
+                                <table id="datatablesSimple">
+                                    <thead>
+                                        <tr>
+                                            <th>Kode Kartu</th>
+                                            <th>Nama Kartu</th>
+                                            <th>Diskon</th>
+                                            <th>Iuran</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <tr>
+                                            <td><?= $kartu['kode'] ?> </td>
+                                            <td><?= $kartu['nama'] ?> </td>
+                                            <td><?= $kartu['diskon'] ?> </td>
+                                            <td><?= $kartu['iuran'] ?> </td>
+                                        </tr>
+                                    </tbody>
+                                    </table>
+</div>
 </div>
