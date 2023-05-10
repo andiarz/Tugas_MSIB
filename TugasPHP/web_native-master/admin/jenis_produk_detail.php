ccc8c710e56747a78d13b07226e83c53cf47697e
Andi Purnomo <106710988+andiarz@users.noreply.github.com>
2023-05-11 02:14:41 +0700

    Update jenis_produk_detail.php

--- TugasPHP/web_native-master/admin/jenis_produk_detail.php
+++ TugasPHP/web_native-master/admin/jenis_produk_detail.php
@@ -5,7 +5,23 @@ $jenis_produk = $model->getJenisProduk($id);
 
 ?>
 
-<div>
-    <h3><?= $produk['nama'] ?> </h3>
-    <h3><?= $produk['keterangan'] ?> </h3>
+<h1 class="mt-4">Tabel Detail Jenis Produk</h1>
+<div class="card-body">
+    <div class="card mb-4">
+                                <table id="datatablesSimple">
+                                    <thead>
+                                        <tr>
+                                            <th>Jenis Produk</th>
+                                            <th>Keteragan</th>
+    
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <tr>
+                                            <td><?= $jenis_produk['nama']?></td>
+                                            <td><?= $jenis_produk['keterangan']?></td>
+                                        </tr>
+                                    </tbody>
+                                    </table>
+</div>
 </div>
